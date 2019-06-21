41fa57deed2c267e5cc08c7c93b79bc2c48e5d96
Michael Dusaniwskyj <dusaniwskyj@gmail.com>
2019-06-21 11:49:54 -0400

    article test complete

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,23 @@
+INSERT INTO blogful_articles (title, date_published, content)
+VALUES
+    ('blogful post 01', now() - '20 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt'),
+    ('blogful post 02', now() - '19 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt'),
+    ('blogful post 03', now() - '18 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt'),
+    ('blogful post 04', now() - '17 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt'),
+    ('blogful post 05', now() - '16 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt'),
+    ('blogful post 06', now() - '15 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt'),
+    ('blogful post 07', now() - '14 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt'),
+    ('blogful post 08', now() - '13 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt'),
+    ('blogful post 09', now() - '12 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt'),
+    ('blogful post 10', now() - '11 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt'),
+    ('blogful post 11', now() - '10 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt'),
+    ('blogful post 12', now() - '09 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt'),
+    ('blogful post 13', now() - '08 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt'),
+    ('blogful post 14', now() - '07 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt'),
+    ('blogful post 15', now() - '06 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt'),
+    ('blogful post 16', now() - '05 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt'),
+    ('blogful post 17', now() - '04 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt'),
+    ('blogful post 18', now() - '03 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt'),
+    ('blogful post 19', now() - '02 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt'),
+    ('blogful post 20', now() - '01 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt')
+    ;
